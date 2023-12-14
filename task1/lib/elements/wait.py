8c3839b05e3aa26b84086d49805f8f3d3beb6806
6ylka99 <boolka.jobs@mail.ru>
2023-12-15 02:50:52 +0300

    Added imports for wait.py file

---
+++ task1/lib/elements/wait.py
@@ -0,0 +1,16 @@
+import datetime
+import logging
+import time
+
+from typing import Callable, Optional, Tuple, Union, List, TypeVar
+
+from selenium.common.exceptions import ElementClickInterceptedException
+from selenium.common.exceptions import ElementNotInteractableException
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
+
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.ui import WebDriverWait
+
+from ..constants import Const
+from .actions import TypeElement
