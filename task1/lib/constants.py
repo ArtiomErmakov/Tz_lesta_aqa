b8cfdf1bef968d3bec3b43e58f5ef9e6e230e2e4
6ylka99 <boolka.jobs@mail.ru>
2023-12-15 02:43:32 +0300

    Added class Const

--- task1/lib/constants.py
+++ task1/lib/constants.py
@@ -2,3 +2,10 @@ class Times:
     TWO_SECONDS: int = 2
     THREE_SECONDS: int = 3
     TEN_SECONDS: int = 10
+
+
+class Const:
+    """Constants storage class."""
+    ELEMENT_WAIT_TIMEOUT: int = 60
+    POLL_FREQUENCY: float = 0.5  # faster frequency consume additional memory!
+    PROPERTY_VALUE: str = "value"
