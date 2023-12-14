60f84d804b832bb6a76686473ca68a99a7b64407
6ylka99 <boolka.jobs@mail.ru>
2023-12-15 02:09:55 +0300

    Added constant TypeElement

--- task1/lib/elements/actions.py
+++ task1/lib/elements/actions.py
@@ -3,3 +3,6 @@ from abc import ABC, abstractmethod
 from typing import List, Optional, Union, NoReturn, Dict, Any, TypeVar, Callable
 
 from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
+
+TypeElement = TypeVar('TypeElement', bound='Element')  # any subclass of Element
+
