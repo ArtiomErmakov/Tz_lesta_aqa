638e9e07b6480bf57e3dffe0b405e04f7aafd562
6ylka99 <boolka.jobs@mail.ru>
2023-12-15 02:11:33 +0300

    Added base abstract class Action for actions

--- task1/lib/elements/actions.py
+++ task1/lib/elements/actions.py
@@ -6,3 +6,52 @@ from selenium.common.exceptions import NoSuchElementException, StaleElementRefer
 
 TypeElement = TypeVar('TypeElement', bound='Element')  # any subclass of Element
 
+
+class IncompleteListActionError(Exception):
+    pass
+
+
+class Action(ABC):
+    """Base abstract class for actions."""
+
+    __slots__ = "_owner", "debug_value"
+
+    def __init__(self, owner: TypeElement) -> None:
+        self._owner: TypeElement = owner
+        self.debug_value: Optional[Any] = None  # return value of the __call__ method, for logging and debugging
+
+    @abstractmethod
+    def __call__(self):
+        pass
+
+    def _continuable_read_list_action(self, method_name: str, *args) -> Any:
+        elements = self._owner.web_element if isinstance(self._owner.web_element, list) else [self._owner.web_element]
+        if self._owner.start_index >= len(elements):
+            # new elements list lesser than was previous
+            # read values from beginning of list
+            start_index = 0
+            self.debug_value = []
+        else:
+            # continue read values from list
+            start_index = self._owner.start_index
+            self.debug_value = self._owner.incomplete_result
+
+        for index, elm in enumerate(elements):
+            # skip values that were already read
+            if index < start_index:
+                continue
+
+            try:
+                attr = getattr(elm, method_name)
+                if isinstance(attr, Callable):
+                    # method
+                    self.debug_value.append(attr(*args))
+                else:
+                    # property
+                    self.debug_value.append(attr)
+            except (NoSuchElementException, StaleElementReferenceException) as err:
+                # save incomplete list and index to Element - for using these values in next reading attempt
+                self._owner.start_index = index
+                self._owner.incomplete_result = self.debug_value
+                raise IncompleteListActionError(f"{index=} {self.debug_value=} {err}")
+        return self.debug_value
