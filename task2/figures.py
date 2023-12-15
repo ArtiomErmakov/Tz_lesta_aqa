8efcf5673dc88b36cbee6241614493329f6460a7
6ylka99 <boolka.jobs@mail.ru>
2023-12-15 09:40:47 +0300

    Added Circle class

--- task2/figures.py
+++ task2/figures.py
@@ -13,3 +13,17 @@ class Figure(ABC):
     @abstractmethod
     def draw(self) -> None:
         pass
+
+
+class Circle(Figure):
+
+    def __init__(self, x, y, canvas, radius, color, draw_img) -> None:
+        super().__init__(x, y, canvas, color, draw_img)
+        self.radius = radius
+
+    def draw(self) -> None:
+        self.canvas.create_oval(self.x, self.y, self.x + self.radius, self.y + self.radius,
+                                fill=self.color,
+                                width=Settings.WIDTH_ZERO)
+        self.draw_img.ellipse((self.x, self.y, self.x + self.radius, self.y + self.radius), fill=self.color)
+        print(f'Drawing Circle at ({self.x}, {self.y}) with radius {self.radius}')
