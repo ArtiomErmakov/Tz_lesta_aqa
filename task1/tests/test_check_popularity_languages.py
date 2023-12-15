4710d4a14ee8b5970b7a63164d25f2d08546ad9b
6ylka99 <boolka.jobs@mail.ru>
2023-12-15 04:51:02 +0300

    Added imports for file

---
+++ task1/tests/test_check_popularity_languages.py
@@ -0,0 +1,6 @@
+import pytest
+
+from selenium.webdriver.remote.webdriver import WebDriver
+from ..lib.server_ui.programming_languages_ui import ProgrammingLanguages, ProgrammingLanguagesUI
+from ..lib import utils
+from typing import List
