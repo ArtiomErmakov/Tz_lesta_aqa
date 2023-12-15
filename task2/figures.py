3e60a8b3293b3b22e84410d87cae32a22572878b
6ylka99 <boolka.jobs@mail.ru>
2023-12-15 09:41:15 +0300

    Added Rectangle class

--- task2/figures.py
+++ task2/figures.py
@@ -27,3 +27,19 @@ class Circle(Figure):
                                 width=Settings.WIDTH_ZERO)
         self.draw_img.ellipse((self.x, self.y, self.x + self.radius, self.y + self.radius), fill=self.color)
         print(f'Drawing Circle at ({self.x}, {self.y}) with radius {self.radius}')
+
+
+class Rectangle(Figure):
+    def __init__(self, x, y, canvas, brush_size, color, draw_img) -> None:
+        super().__init__(x, y, canvas, color, draw_img)
+        self.height = int(brush_size / 2)
+        self.width = brush_size
+
+    def draw(self) -> None:
+        self.canvas.create_rectangle(self.x, self.y, self.x + self.width, self.y + self.height,
+                                     fill=self.color,
+                                     width=Settings.WIDTH_ZERO)
+        self.draw_img.polygon((self.x, self.y, self.x + self.width, self.y, self.x + self.width,
+                               self.y + self.height, self.x, self.y + self.height),
+                              fill=self.color)
+        print(f'Drawing Rectangle at ({self.x}, {self.y}) with width {self.width}, height {self.height}')
