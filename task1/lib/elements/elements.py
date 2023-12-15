5fc5b1da7145825599b9143ef97583793adec422
6ylka99 <boolka.jobs@mail.ru>
2023-12-15 03:04:37 +0300

    Added imports for elements.py file

---
+++ task1/lib/elements/elements.py
@@ -0,0 +1,24 @@
+import datetime
+import logging
+import time
+from typing import Tuple, Union, NoReturn, Optional, Any, List, Dict, TypeVar, Callable
+
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.remote.webelement import WebElement
+
+
+from .actions import ClearAction, ClickAction, ReadTextAction, SendKeysAction, IsOnPageAction, GetLenAction, Action
+from .actions import IsEnabledAction, ClickByIndexElementsAction, IsSelectedByIndexElementsAction, SubmitAction
+from .actions import WaitTextAction, IsSelectedAction, ReadTextListAction, GetAttributeAction, GetPropertyAction
+from .actions import WaitTextChangedAction, GetSizeAction, ReadPropertyListAction, ReadAttributeListAction
+from .actions import IncompleteListActionError, TypeElement
+from .wait import IGNORED_EXCEPTIONS
+from .wait import WebDriverWaitTill
+from ..constants import Const, Times
+from ..locators.locator import Locator
