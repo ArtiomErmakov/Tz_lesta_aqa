77a53503b90bb929bf5827b0ce6a93ef099073a6
6ylka99 <boolka.jobs@mail.ru>
2023-12-15 06:56:12 +0300

    Fixed return type for driver fixture

--- task1/conftest.py
+++ task1/conftest.py
@@ -1,10 +1,11 @@
 import pytest
 from selenium import webdriver
 from typing import Any, Generator
+from selenium.webdriver.remote.webdriver import WebDriver
 
 
 @pytest.fixture(scope="session", autouse=True)
-def driver() -> Generator[webdriver.Chrome, Any, None]:
+def driver() -> Generator[WebDriver, Any, None]:
     driver = webdriver.Chrome()
     yield driver
     driver.quit()
