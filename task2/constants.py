bc4576c4d4a550f9b761a66f2c4ba03f99d00bb8
6ylka99 <boolka.jobs@mail.ru>
2023-12-15 09:36:04 +0300

    Added Settings class

---
+++ task2/constants.py
@@ -0,0 +1,44 @@
+class Settings:
+
+    SIZE_1280x720: str = '1280x720'
+    SIZE_TEN: int = 10
+
+    POINT_ZERO: int = 0
+    POINT_1280: int = 1280
+    POINT_720: int = 720
+    POINT_640: int = 640
+
+    WIDTH_ZERO: int = 0
+    WIDTH_TEN: int = 10
+
+    WEIGHT_ONE: int = 1
+    WEIGHT_ELEVEN: int = 11
+    WEIGHT_TEN: int = 10
+
+    SIX_COLUMN: int = 6
+    ONE_COLUMN: int = 1
+    TWO_COLUMN: int = 2
+    THREE_COLUMN: int = 3
+    TWO_ROW: int = 2
+    ZERO_COLUMN: int = 0
+    ZERO_ROW: int = 0
+    SEVEN_COLUMN: int = 7
+    ONE_ROW: int = 1
+
+    FIVE_PIXELS: int = 5
+    SIX_PIXELS: int = 6
+
+    BUTTON_B1: str = '<B1-Motion>'
+    BUTTON_3: str = '<Button-3>'
+
+    COLON: str = ':'
+
+    VALUE_ONE: int = 1
+    VALUE_ONE_HUNDRED: int = 100
+    TEN_VALUE: int = 10
+
+    ALL: str = 'all'
+
+    COLUMN_BG: str = 'bg'
+
+    FALSE_VALUE: bool = False
