1b08ee7671685f1d21ba4478a7450a19878bb05c
6ylka99 <boolka.jobs@mail.ru>
2023-12-15 09:41:43 +0300

    Added Triangle class

--- task2/figures.py
+++ task2/figures.py
@@ -43,3 +43,22 @@ class Rectangle(Figure):
                                self.y + self.height, self.x, self.y + self.height),
                               fill=self.color)
         print(f'Drawing Rectangle at ({self.x}, {self.y}) with width {self.width}, height {self.height}')
+
+
+class Triangle(Figure):
+
+    def __init__(self, x, y, canvas, size, color, draw_img) -> None:
+        super().__init__(x, y, canvas, color, draw_img)
+        self.size = size
+
+    def draw(self) -> None:
+        self.canvas.create_polygon([self.x, self.y],
+                                   [self.x + self.size / 2, self.y + self.size],
+                                   [self.x - self.size / 2, self.y + self.size],
+                                   fill=self.color)
+        self.draw_img.polygon([(self.x, self.y),
+                               (self.x + self.size / 2, self.y + self.size),
+                               (self.x - self.size / 2, self.y + self.size)],
+                              fill=self.color)
+        print(f'Drawing Triangle with vertices [({self.x}, {self.y}), ({int(self.x + self.size / 2)}, '
+              f'{self.y + self.size}), ({int(self.x - self.size / 2)}, {self.y + self.size})]')
