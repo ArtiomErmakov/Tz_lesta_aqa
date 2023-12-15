a82f706cfe20259a54a69114acf87d9955a2343f
6ylka99 <boolka.jobs@mail.ru>
2023-12-15 04:52:04 +0300

    Added test class TestCheckPopularityLanguages

--- task1/tests/test_check_popularity_languages.py
+++ task1/tests/test_check_popularity_languages.py
@@ -4,3 +4,6 @@ from selenium.webdriver.remote.webdriver import WebDriver
 from ..lib.server_ui.programming_languages_ui import ProgrammingLanguages, ProgrammingLanguagesUI
 from ..lib import utils
 from typing import List
+
+
+class TestCheckPopularityLanguages:
